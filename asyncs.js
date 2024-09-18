412fc5aafb7ef52e2503dd63ce6a9f53cb14998d
Dev-Devant <andeliros@yahoo.com.ar>
2024-09-18 13:19:08 -0300

    Update asyncs.js

--- asyncs.js
+++ asyncs.js
@@ -1,4 +1,4 @@
-const server = "https://artekaimogo-production.up.railway.app"
+const server = "https://artekaimogo-production.up.railway.app:8080"
 
 // Función para realizar una solicitud POST
 async function postData(url = "", data = {}) {
